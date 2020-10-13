cd5688b259643aaebed7b3b6cb4b08501ac5b787
Hugin Antony <huginantony@gmail.com>
2020-10-13 23:30:27 +0200

    Adding validators for Event dates

--- Tournament.Core/Validators/EventValidator.cs
+++ Tournament.Core/Validators/EventValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
 using Tournament.Core.Models;
 
 namespace Tournament.Core.Validators
@@ -7,6 +9,8 @@ namespace Tournament.Core.Validators
     {
         public EventValidator()
         {
+            DateTime eventDateTime = DateTime.Now;
+            
             RuleFor(x => x.TournamentId)
                 .NotEmpty()
                 .WithMessage("tournamentId is required.")
@@ -28,10 +32,26 @@ namespace Tournament.Core.Validators
                 .WithMessage("eventDateTime is required.")
                 .OverridePropertyName("eventDateTime");
 
+            RuleFor(x => x.EventDateTime)
+                .Custom((d, context) =>
+                {
+                    eventDateTime = d;
+                    if (d.Date <= DateTime.Now.Date.AddDays(-1))
+                        context.AddFailure(new ValidationFailure("eventDateTime", $"eventDateTime cannot be before today"));
+                });
+
             RuleFor(x => x.EventEndDateTime)
                 .NotEmpty()
                 .WithMessage("eventEndDateTime is required.")
                 .OverridePropertyName("eventEndDateTime");
+
+            RuleFor(x => x.EventDateTime)
+                .Custom((d, context) =>
+                {
+                    
+                    if (d.Date <= eventDateTime.Date)
+                        context.AddFailure(new ValidationFailure("eventEndDateTime", $"eventEndDateTime cannot be before the eventDateTime"));
+                });
 
             RuleFor(x => x.AutoClose)
                 .NotEmpty()
